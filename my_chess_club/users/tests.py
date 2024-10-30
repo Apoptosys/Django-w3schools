a61774185b39e359ac70d802743b8e036972334a
Apoptosys <zaharia_catalin0337@yahoo.com>
2024-10-30 22:15:45 +0200

    Tests for user and profile models

--- my_chess_club/users/tests.py
+++ my_chess_club/users/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import User, Profile
 
-# Create your tests here.
+
+class TestModels(TestCase):
+    def test_user_model(self):
+        u = User.objects.create(
+            username="testuser", email="testuser@user.com", password="secret"
+        )
+        self.assertEqual(str(u), "testuser")
+
+    def test_profile_model(self):
+        """
+        Tests that the Profile model is created when a User is created, as specified in signals.py.
+        Also tests that the __str__ method returns the username of the User.
+        """
+        u = User.objects.create(
+            username="testuser", email="testuser@user.com", password="secret"
+        )
+        p = u.profile
+        self.assertEqual(str(p), "testuser")
+
+
